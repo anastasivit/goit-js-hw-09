2597eb1f38503a19f507f695bf4bdf14722f42a3
anastasivit <119945000+anastasivit@users.noreply.github.com>
2023-05-11 12:52:27 +0300

    Add 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+document.addEventListener('DOMContentLoaded', function (event) {
+  const startButton = document.querySelector('[data-start]');
+  const stopButton = document.querySelector('[data-stop]');
+  let intervalId = null;
 
+  function startChangingColor() {
+    startButton.disabled = true;
+    intervalId = setInterval(function () {
+      document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  }
+
+  function stopChangingColor() {
+    startButton.disabled = false;
+    clearInterval(intervalId);
+  }
+
+  startButton.addEventListener('click', startChangingColor);
+  stopButton.addEventListener('click', stopChangingColor);
+
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  }
+});
